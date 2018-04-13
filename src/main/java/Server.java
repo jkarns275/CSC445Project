a56f9879dc685ef61c9c821994787f6988b77ea4
Benjamin Valentino <brv545@gmail.com>
2018-04-13 15:49:19 -0400

    first commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,12 @@
+public class Server {
+
+    public static void main(String[] args) {
+        new Server().listen();
+    }
+
+    public void listen() {
+        while(true) {
+
+        }
+    }
+}
